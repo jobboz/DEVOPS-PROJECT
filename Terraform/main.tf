8cc9d0e599b741fa9494e089411440e64093847a
jobboz <josephjobbos@gmail.com>
2024-08-23 11:42:40 +0100

    DEPLOY INFRA

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -83,10 +83,6 @@ resource "aws_security_group" "maingroup" {
 }
 
 
-
-
-
-
 resource "aws_key_pair" "deployer" {
     key_name = var.key_name
     public_key = var.public_key
--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -3,12 +3,14 @@ variable "region" {
   
 }
 
+
 variable "public_key" {
   
+
 }
 
 variable "private_key" {
-  
+
 }
 
 variable "key_name" {
