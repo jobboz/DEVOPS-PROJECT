31935fdf1eb18dc1524d3e18443ba40147db8c62
jobboz <josephjobbos@gmail.com>
2024-08-25 14:48:46 +0100

    dep

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -59,48 +59,44 @@ resource "aws_security_group" "maingroup" {
     ]
 
     ingress = [
-          {
-            cidr_blocks      = ["0.0.0.0/0", ]
-            description      = ""
-            from_port        = 22
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "tcp"
-            security_groups  = []
-            self = false
-            to_port = 22
-        },
-          {
-            cidr_blocks      = ["0.0.0.0/0", ]
-            description      = ""
-            from_port        = 80
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            protocol         = "tcp"
-            security_groups  = []
-            self             = false
-            to_port          = 80
-        }
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
 
-    ]
-  
 }
 
 
 
 resource "aws_key_pair" "deployer" {
-    key_name = var.key_name
-    public_key = var.public_key
-  
+  key_name   = var.key_name
+  public_key = var.public_key
 }
-
-
 
 output "instance_public_ip" {
-    value = aws_instance.server.public_ip
-    sensitive = true
-}
+  value     = aws_instance.servernode.public_ip
+  sensitive = true
 
+}
 
 
 
