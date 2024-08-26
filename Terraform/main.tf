105ab5411491fa6f7c7150422930ac955aea773a
jobboz <josephjobbos@gmail.com>
2024-08-26 08:43:24 +0100

    modified main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -102,9 +102,9 @@ output "instance_public_ip" {
     sensitive = true
 }
 
-output "instance_id" {
-  value = aws_instance.my_instance.id
-}
+# output "instance_id" {
+#   value = aws_instance.my_instance.id
+# }
 
 
 
